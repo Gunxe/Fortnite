09c3f9c24f6cebdc734cb29eb7b737a621f9edd3
Leandro Gilomen <l.gilomen@limo-bern.ch>
2021-07-07 10:39:50 +0200

    JSOBParserFinish

--- app/src/main/java/ch/bbcag/fortnite/adapter/ShopAdapter.java
+++ app/src/main/java/ch/bbcag/fortnite/adapter/ShopAdapter.java
@@ -2,16 +2,22 @@ package ch.bbcag.fortnite.adapter;
 
 import android.content.Context;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+import ch.bbcag.fortnite.R;
 import ch.bbcag.fortnite.model.Bundles;
 
 public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ShopViewHolder> {
@@ -27,12 +33,15 @@ public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ShopViewHolder
     @NotNull
     @Override
     public ShopViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view= inflater.inflate(R.layout.shop_row, parent, false);
+        return new ShopViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ShopViewHolder holder, int position) {
-
+        holder.preis.setText(bundles.get(position).getPrice());
+        Picasso.get().load(bundles.get(position).getImageURL()).into(holder.iconImage);
     }
 
     @Override
@@ -41,8 +50,13 @@ public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ShopViewHolder
     }
 
     public class ShopViewHolder extends RecyclerView.ViewHolder{
+        TextView preis;
+        ImageView iconImage;
+
         public ShopViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            preis = itemView.findViewById(R.id.bundlePrice);
+            iconImage = itemView.findViewById(R.id.SkinIcon);
         }
     }
 
