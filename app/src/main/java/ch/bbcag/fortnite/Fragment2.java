98785663360f5a4286a8cafeb36b9a7c96a9c74d
sha122680 <sha122680@iet-gibb.ch>
2021-07-07 16:20:18 +0200

    news fertig (lele isch eh dulli)

--- app/src/main/java/ch/bbcag/fortnite/Fragment2.java
+++ app/src/main/java/ch/bbcag/fortnite/Fragment2.java
@@ -1,5 +1,7 @@
 package ch.bbcag.fortnite;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,10 +10,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,46 +30,74 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.bbcag.fortnite.adapter.NewsAdapter;
 import ch.bbcag.fortnite.helper.NewsJSONParser;
+import ch.bbcag.fortnite.model.Bundles;
 import ch.bbcag.fortnite.model.News;
 
 public class Fragment2 extends Fragment {
-    private ListView articles;
-    private static String NEWS_URL = "https://fortnite-api.com/v2/news";
+    List<News> articles = new ArrayList<News>();
+    private static final String NEWS_URL = "https://fortnite-api.com/v2/news";
+    ProgressBar progressBar;
+    RecyclerView recyclerView;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        addArticlesToList();
         return inflater.inflate(R.layout.fragment2_layout, container, false);
     }
 
-    private void addArticlesToList() {
-        String url = "https://fortnite-api.com/v2/news";
+    @Override
+    public void onStart() {
+        super.onStart();
+        progressBar = getView().findViewById(R.id.loading_news_progress);
+        recyclerView = getView().findViewById(R.id.recyclerView_news);
+        getNews();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        NewsAdapter newsAdapter = new NewsAdapter(getContext(), articles);
+        recyclerView.setAdapter(newsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        textView.setText("Response: " + response.toString());
-                    }
-                }, new Response.ErrorListener() {
+    private void getNews() {
+        RequestQueue queue = Volley.newRequestQueue(getContext().getApplicationContext());
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, NEWS_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    articles.addAll(NewsJSONParser.createArticleFromJsonString(response));
+                    progressBar.setVisibility(View.GONE);
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                generateAlertDialog();
+            }
+        });
+        queue.add(objectRequest);
+    }
 
-                    }
-                });
-
-        // Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-
-        getView().findViewById(R.id.articleList);
-        ArrayAdapter<News> newsAdapter = new ArrayAdapter<News>(getView().getContext().getApplicationContext(), android.R.layout.simple_list_item_1);
-        newsAdapter.addAll(NewsJSONParser.createArticleFromJsonString());
-        articles.setAdapter(newsAdapter);
+    private void generateAlertDialog() {
+        progressBar.setVisibility(View.GONE);
+        AlertDialog.Builder dialogBuilder;
+        dialogBuilder = new AlertDialog.Builder(getContext());
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getActivity().finish();
+            }
+        });
+        dialogBuilder.setMessage("Die News konnten nicht geladen werden. Versuche es später nochmals.").setTitle("Fehler");
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
